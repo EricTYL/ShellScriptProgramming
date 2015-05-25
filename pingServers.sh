8ba251d23c26b8c6f6437941561daf409b05ebdf
RickyTYL <rickytyl@icloud.com>
2015-05-25 20:40:20 +0800

    Add pingServers.sh

---
+++ pingServers.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+ 
+LOG=/tmp/mylog.log 
+SECONDS=60 
+
+ 
+for i in $@; do 
+    echo "$i-UP!" > $LOG.$i 
+
+done 
+   
+while true; do 
+  for i in $@; do 
+
+    ping -c 1 $i > /dev/null 
+      if [ $? -ne 0 ]; then 
+        STATUS=$(cat $LOG.$i) 
+        if [ $STATUS != "$i-DOWN!" ]; then 
+          echo "`date`: ping failed, $i host is down!" 
+        fi 
+          echo "$i-DOWN!" > $LOG.$i 
+  
+      else 
+        STATUS=$(cat $LOG.$i)
+        if [ $STATUS != "$i-UP!" ]; then 
+          echo "`date`: ping OK, $i host is up!" 
+        fi 
+          echo "$i-UP!" > $LOG.$i 
+      fi 
+  done 
+
+sleep $SECONDS 
+done
